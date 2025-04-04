80cbe81c14e9b38a68ec9bcfe00278d9dda8dd12
Braunerr <mathn519@gmail.com>
2025-04-04 23:09:31 +0200

    Remove unused PrintTable function to clean up code

--- MrMythical.lua
+++ MrMythical.lua
@@ -307,18 +307,6 @@ end
 ---------------------
 -- Helper Functions --
 ---------------------
-local function PrintTable(t, indent)
-    indent = indent or ""
-    for k, v in pairs(t) do
-        if type(v) == "table" then
-            print(indent .. tostring(k) .. ":")
-            PrintTable(v, indent .. "  ")
-        else
-            print(indent .. tostring(k) .. ": " .. tostring(v))
-        end
-    end
-end
-
 function GetRegionForRaiderIO()
     if GetCurrentRegion then
         local regNum = GetCurrentRegion()
