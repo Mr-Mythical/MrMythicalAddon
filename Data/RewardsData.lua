1f9d6cdb916e57b74bcdc1f66345deaac97e944c
Braüner <mathn519@gmail.com>
2026-03-01 18:19:20 +0100

    Updated rewards info to S1

--- Data/RewardsData.lua
+++ Data/RewardsData.lua
@@ -10,41 +10,41 @@ local MrMythical = MrMythical or {}
 
 MrMythical.RewardsData = {
     DUNGEON_GEAR = {
-        { itemLevel = 684, upgradeTrack = "Champion 2" },  -- Key Level 2
-        { itemLevel = 684, upgradeTrack = "Champion 2" },  -- Key Level 3
-        { itemLevel = 688, upgradeTrack = "Champion 3" },  -- Key Level 4
-        { itemLevel = 691, upgradeTrack = "Champion 4" },  -- Key Level 5
-        { itemLevel = 694, upgradeTrack = "Hero 1" },      -- Key Level 6
-        { itemLevel = 694, upgradeTrack = "Hero 1" },      -- Key Level 7
-        { itemLevel = 697, upgradeTrack = "Hero 2" },      -- Key Level 8
-        { itemLevel = 697, upgradeTrack = "Hero 2" },      -- Key Level 9
-        { itemLevel = 701, upgradeTrack = "Hero 3" }       -- Key Level 10+
+        { itemLevel = 250, upgradeTrack = "Champion 2" },  -- Key Level 2
+        { itemLevel = 250, upgradeTrack = "Champion 2" },  -- Key Level 3
+        { itemLevel = 253, upgradeTrack = "Champion 3" },  -- Key Level 4
+        { itemLevel = 256, upgradeTrack = "Champion 4" },  -- Key Level 5
+        { itemLevel = 259, upgradeTrack = "Hero 1" },      -- Key Level 6
+        { itemLevel = 259, upgradeTrack = "Hero 1" },      -- Key Level 7
+        { itemLevel = 263, upgradeTrack = "Hero 2" },      -- Key Level 8
+        { itemLevel = 263, upgradeTrack = "Hero 2" },      -- Key Level 9
+        { itemLevel = 266, upgradeTrack = "Hero 3" }       -- Key Level 10+
     },
 
     VAULT_GEAR = {
-        { itemLevel = 694, upgradeTrack = "Hero 1" },      -- Key Level 2
-        { itemLevel = 694, upgradeTrack = "Hero 1" },      -- Key Level 3
-        { itemLevel = 697, upgradeTrack = "Hero 2" },      -- Key Level 4
-        { itemLevel = 697, upgradeTrack = "Hero 2" },      -- Key Level 5
-        { itemLevel = 701, upgradeTrack = "Hero 3" },      -- Key Level 6
-        { itemLevel = 704, upgradeTrack = "Hero 4" },      -- Key Level 7
-        { itemLevel = 704, upgradeTrack = "Hero 4" },      -- Key Level 8
-        { itemLevel = 704, upgradeTrack = "Hero 4" },      -- Key Level 9
-        { itemLevel = 707, upgradeTrack = "Myth 1" }       -- Key Level 10+
+        { itemLevel = 259, upgradeTrack = "Hero 1" },      -- Key Level 2
+        { itemLevel = 259, upgradeTrack = "Hero 1" },      -- Key Level 3
+        { itemLevel = 263, upgradeTrack = "Hero 2" },      -- Key Level 4
+        { itemLevel = 263, upgradeTrack = "Hero 2" },      -- Key Level 5
+        { itemLevel = 266, upgradeTrack = "Hero 3" },      -- Key Level 6
+        { itemLevel = 269, upgradeTrack = "Hero 3" },      -- Key Level 7
+        { itemLevel = 269, upgradeTrack = "Hero 3" },      -- Key Level 8
+        { itemLevel = 269, upgradeTrack = "Hero 3" },      -- Key Level 9
+        { itemLevel = 272, upgradeTrack = "Hero 3" }       -- Key Level 10+
     },
 
     CRESTS = {
-        { crestType = "Runed", amount = 10 },   -- Key Level 2
-        { crestType = "Runed", amount = 12 },   -- Key Level 3
-        { crestType = "Runed", amount = 14 },   -- Key Level 4
-        { crestType = "Runed", amount = 16 },   -- Key Level 5
-        { crestType = "Runed", amount = 18 },   -- Key Level 6
-        { crestType = "Gilded", amount = 10 },  -- Key Level 7
-        { crestType = "Gilded", amount = 12 },  -- Key Level 8
-        { crestType = "Gilded", amount = 14 },  -- Key Level 9
-        { crestType = "Gilded", amount = 16 },  -- Key Level 10
-        { crestType = "Gilded", amount = 18 },  -- Key Level 11
-        { crestType = "Gilded", amount = 20 },  -- Key Level 12+
+        { crestType = "Hero", amount = 10 },   -- Key Level 2
+        { crestType = "Hero", amount = 12 },   -- Key Level 3
+        { crestType = "Hero", amount = 14 },   -- Key Level 4
+        { crestType = "Hero", amount = 16 },   -- Key Level 5
+        { crestType = "Hero", amount = 18 },   -- Key Level 6
+        { crestType = "Myth", amount = 10 },  -- Key Level 7
+        { crestType = "Myth", amount = 12 },  -- Key Level 8
+        { crestType = "Myth", amount = 14 },  -- Key Level 9
+        { crestType = "Myth", amount = 16 },  -- Key Level 10
+        { crestType = "Myth", amount = 18 },  -- Key Level 11
+        { crestType = "Myth", amount = 20 },  -- Key Level 12+
     }
 }
 
